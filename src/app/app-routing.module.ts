be9df4803b4f212a17084f04330c6e544053eaa7
asakon <kondoasako@gmail.com>
2018-06-25 12:27:10 +0900

    ChannelComponentのルート設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChannelComponent } from './channel/channel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'channel/:name',
+    component: ChannelComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
